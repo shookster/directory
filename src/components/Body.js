6ac8d3b138ea2e88255d4de55922da248e2ef938
Ethan Shook <eshook89@gmail.com>
2020-12-01 13:17:38 -0600

    have working api request

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Component } from "react";
 import Navbar from "./Navbar.js";
 import EmployeeTable from "./EmployeeTable.js"
+import API from "../utils/API.js";
 
 export default class Body extends Component {
     state= {
@@ -12,16 +13,80 @@ export default class Body extends Component {
     heading = [
         "Image", "Name", "Phone", "Email", "DOB"
     ]
-    handleSearch = event => {
-        console.log(event.target.value)
+    handleSearch = heading => {
+        console.log(this.target.value)
+        if (this.state.order === "descending") {
+            this.setState({
+                order: "ascending"
+            })
+        } else {
+            this.setState({
+                order: "descending"
+            })
+        } 
+        
+        const compareFnc = (a, b) => {
+            if (this.state.order === "ascend") {
+                if (a[heading] === undefined) {
+                    return 1;
+                } else if (b[heading] === undefined) {
+                    return -1;
+                }
+                else if (heading === "name") {
+                    return a[heading].first.localeCompare(b[heading].first);
+                } else {
+                    return a[heading] - b[heading];
+                }
+            } else {
+                if (a[heading] === undefined) {
+                    return 1;
+                } else if (b[heading] === undefined) {
+                    return -1;
+                }
+                else if (heading === "name") {
+                    return b[heading].first.localeCompare(a[heading].first);
+                } else {
+                    return b[heading] - a[heading];
+                }
+            }
+        }
+        const sortedUsers = this.state.sortedUsers.sort(compareFnc);
+        this.setState({ sortedUsers: sortedUsers });
     }
+
+    handleSearchChange = event => {
+        console.log(event.target.value);
+        const filter = event.target.value;
+        const filteredList = this.state.users.filter(item => {
+            let values = Object.values(item)
+            .join("")
+            .toLowerCase();
+        return values.indexOf(filter.toLowerCase()) !== -1;
+        })
+        this.setState({ sortedUsers: filteredList });
+    }
+    componentDidMount() {
+        API.getUsers().then(results => {
+            this.setState({
+                users: results.data.results,
+                sortedUsers: results.data.results
+            });
+        });
+    }
+
     render() {
         return (
             <>
             <Navbar handleSearch = {this.handleSearch}/>
-            <EmployeeTable heading = {this.heading}/>
+            <div className="data-area">
+            <EmployeeTable 
+                heading = {this.heading}
+                users={this.state.sortedUsers}
+                handleSort={this.handleSort}
+            />
+            </div>
             </>
 
-        )
+        );
     }
 }
--- src/components/EmployeeTable.js
+++ src/components/EmployeeTable.js
@@ -1,23 +1,69 @@
 import React from "react";
 
-function EmployeeTable ({ heading}) {
+function EmployeeData({ users }) {
+    function formatDate(date) {
+        const dateArray = date.split("-");
+        const year = dateArray[0];
+        const month = dateArray[1];
+        const dayArray = dateArray[2].split("T");
+        const day = dayArray[0];
+        const formattedDate = [month, day, year].join("-");
+        return formattedDate;
+    }
+
+
+// function EmployeeTable ({ heading}) {
+//     return (
+//             <table>
+//                 <thead>
+//                     <tr>
+//                         {heading.map(({name})=> {
+//                             return (
+//                                 <th>
+//                                     {name}
+//                                 </th>
+//                             )
+//                         })}
+//                     </tr>
+//                 </thead>
+//                 <tbody>
     return (
-        <div>
-            <table>
-                <thead>
-                    <tr>
-                        {heading.map(({name})=> {
+        <tbody>
+                    {users[0] !== undefined && users[0].name !== undefined ? (
+                        users.map(({ login, name, picture, phone, email, dob}) => {
                             return (
-                                <th>
-                                    {name}
-                                </th>
-                            )
-                        })}
-                    </tr>
-                </thead>
-            </table>
-        </div>
+                                <tr key={login.uuid}>
+                                    <td dat-th="Image" className="align-middle">
+                                        <img
+                                        src={picture.medium}
+                                        alt={"profile image for " + name.first + " " + name.last}
+                                        className="img-responsive"
+                                        />
+                                    </td>
+                                    <td dat-th="Phone" className="align-middle">
+                                        {name.first} {name.last}
+                                    </td>
+                                    <td data-th="Phone" className="align-middle">
+                                        {phone}
+                                    </td>
+                                    <td dat-th="Email" className="align-middle">
+                                        <a href={"mailto:" + email} target="__blank">
+                                            {email}
+                                        </a>
+                                    </td>
+                                    <td data-th="DOB" className="align-middle">
+                                        {formatDate(dob.date)}
+                                    </td>
+                                </tr>
+                            );
+                        })
+                    ) : (
+                        <>
+                        </>
+                    )}
+             </tbody>
+            // </table>
     )
 }
 
-export default EmployeeTable
+export default EmployeeData
